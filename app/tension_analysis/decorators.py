a78cba959b5027cb69f21f65e97dab1639f77bf0
Ling-Xiao Yang <ling@theyang.ca>
2019-07-03 20:54:23 -0400

    New: set up the framework of user cookies

---
+++ app/tension_analysis/decorators.py
@@ -0,0 +1,20 @@
+from functools import wraps
+import re
+import uuid
+
+from flask import current_app, g, make_response, request
+
+RE_UUID_HEX = re.compile('^[0-9a-f]{32}$')
+
+
+def ensure_user_cookie(f):
+    @wraps(f)
+    def decorated_function(*a, **k):
+        cookie_name = current_app.config['USER_IDENTIFICATION_COOKIE_NAME']
+        g.user_id = request.cookies.get(cookie_name)
+        if not RE_UUID_HEX.match(str(g.user_id)):
+            g.user_id = uuid.uuid4().hex
+        response = make_response(f(*a, **k))
+        response.set_cookie(cookie_name, g.user_id)
+        return response
+    return decorated_function
--- app/tension_analysis/views.py
+++ app/tension_analysis/views.py
@@ -1,8 +1,36 @@
-from flask import Blueprint, render_template
+from pathlib import Path
+
+from flask import Blueprint, current_app, g, make_response, render_template, request
+
+from .decorators import ensure_user_cookie
 
 views = Blueprint('views', __name__)
 
+# Result file storage
+RESULTS_PATH = Path('/tmp/tension_analysis_results')
+if not RESULTS_PATH.is_dir():
+    RESULTS_PATH.mkdir(parents=True)
 
-@views.route('/')
-def hello():
-    return "Hello World!"
+
+@views.route('/', methods=['GET', 'POST'])
+@ensure_user_cookie
+def welcome():
+    errors = {}
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            errors['file'] = 'File missing'
+        else:
+            input_file = request.files['file']
+            print(input_file)
+    return render_template('welcome.html', errors=errors)
+
+@views.route('/result/')
+@ensure_user_cookie
+def result():
+    return "TEST"
+
+@views.route('/result.csv')
+@ensure_user_cookie
+def result_csv():
+    return "TEST"
