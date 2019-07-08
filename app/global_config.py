e794cff93be9a9b2846d4a5739ae4e888280bc1a
Ling-Xiao Yang <ling@theyang.ca>
2019-07-08 17:32:08 -0400

    New: background processing

--- app/global_config.py
+++ app/global_config.py
@@ -6,3 +6,8 @@ DATA_ROOT = '/srv/data'
 HEDGE_DETECTION_THRESHOLD = 0.8
 STORAGE_PATH = '/mnt/tension_analysis_results'
 USER_IDENTIFICATION_COOKIE_NAME = 'uid'
+
+
+import logging  # noqa
+logger = logging.getLogger('tension_analysis')
+logger.setLevel(logging.INFO)
--- app/run_worker.py
+++ app/run_worker.py
@@ -1,21 +1,20 @@
-import logging
 import time
 import traceback
 
+from global_config import logger
 from storage import take_from_queue, NothingTaken
 from tension_analysis_worker import task_tension_analysis
 
-logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logger.info('Worker ready.')
     while True:
         try:
             with take_from_queue() as user_id:
-                logger.info('Taking {}'.format(user_id))
+                logger.info('Worker is taking {}'.format(user_id))
                 task_tension_analysis(user_id)
         except NothingTaken:
             time.sleep(2)
         except Exception as e:
-            logger.error('Unexpected exception:')
+            logger.error('Unexpected exception. Worker quitting:')
             logger.error(traceback.format_exc())
